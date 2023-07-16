48409a1c2dd687c8a1b4da1987d7132f560426df
kaminur1 <139678086+kaminur1@users.noreply.github.com>
2023-07-16 20:36:51 +0600

    Update Customer.java

--- Customer.java
+++ Customer.java
@@ -1,2 +1,15 @@
 public class Customer {
+  int wallet_id;
+    int age;
+    String name;
+    String city;
+    String address;
+    String cart_id;
+    int item_id{
+        void review_item();
+    }
+    void buy_from_cart();
+     for(double i=0; i<=5.0; i++){
+        double raiting=i;
+    }
 }
