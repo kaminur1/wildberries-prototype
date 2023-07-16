b41590e22f5d5a945ad7b50ac39429c30980cf9c
kaminur1 <139678086+kaminur1@users.noreply.github.com>
2023-07-16 19:35:08 +0600

    Update Seller.java

--- Seller.java
+++ Seller.java
@@ -1,2 +1,15 @@
 public class Seller {
+  int shop_id;
+  int wallet_id;
+  for(double i=0; i<=5.0; i++){
+      double raiting=i;
+    }
+  void add_item{
+      String item = "Item";
+    }
+  int item_id{
+      void delete_item;
+    void change_quantity;
+        void change_price;
+    }
 }
